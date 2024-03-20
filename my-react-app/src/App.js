9d358def4aa9a2b2ce1bb55e4773b458049c1e19
justinravenwolfe <147559260+justinravenwolfe@users.noreply.github.com>
2024-03-19 22:27:19 -0400

    made contat page and form Co-authored-by: Alliyah Steele <HelloUniversePyC@users.noreply.github.com>

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header.js';
 import AboutMe from './components/AboutMe.js';
 import Portfolio from './components/Porfolio.js'; 
+import Contact from './components/Contact.js'; 
 import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<AboutMe />} />
         <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </div>
   );
--- my-react-app/src/components/Header.js
+++ my-react-app/src/components/Header.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {Navbar} from 'react-bootstrap'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const Header = () => {
 //HTML 
@@ -10,7 +12,7 @@ const Header = () => {
             <img src = "https://upload.wikimedia.org/wikipedia/en/thumb/1/1e/Baseball_%28crop%29.jpg/1200px-Baseball_%28crop%29.jpg" alt = "baseball" height = "30" width = "30"></img>
             <li><a href = "#about-me">About Me</a></li> 
             <li><a href = "/portfolio">Portfolio</a></li>
-            <li><a href = "#contact">Contact</a></li>
+            <li><a href = "/contact">Contact</a></li>
             <li><a href = "#resume">Resume</a></li>
         </Navbar>
         </header>

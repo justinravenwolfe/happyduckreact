ea322026e2c58fa8fba3e71be243a6571bd6494d
justinravenwolfe <147559260+justinravenwolfe@users.noreply.github.com>
2024-03-29 16:17:37 -0400

    added picture

--- my-react-app/src/components/AboutMe.js
+++ my-react-app/src/components/AboutMe.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import JustinFinal from '../images/JustinFinal.jpg'; // Correct the path if needed
+import 'bootstrap/dist/css/bootstrap.min.css'; // Correct the path if needed
 
 const AboutMe = () => {
     return (
     <section id="about-me">
         <h2>About ME</h2>
-        <img src={JustinFinal} alt="justin ramirez" />
+        <img src='https://t4.ftcdn.net/jpg/02/36/62/99/360_F_236629968_cvZT3xpLsf26tuvcJOLaU8xIR1GfPrus.jpg' alt="justin ramirez" />
         <p>Coding student and proud dog dad. Educated at Rutgers for undergrad and full stack. I enjoy helping animals 
           keeping up to date with new technologies </p>
     </section>
--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.js';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(
+// Use createRoot instead of ReactDOM.render
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
       <App />
     </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );

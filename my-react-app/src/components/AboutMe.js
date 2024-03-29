729ba824b338ff1a278431868afbcce6f3d9da8e
justinravenwolfe <147559260+justinravenwolfe@users.noreply.github.com>
2024-03-29 16:08:15 -0400

    making changes to aabout me
    Co-authored-by: Alliyah Steele <HelloUniversePyC@users.noreply.github.com>

--- my-react-app/src/components/AboutMe.js
+++ my-react-app/src/components/AboutMe.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import JustinFinal from '../images/JustinFinal.jpg'; // Correct the path if needed
 
 const AboutMe = () => {
     return (
-    <section id = "about-me">
-      <h2> About ME </h2>
-      <img src= "https://usa.bootcampcdn.com/wp-content/uploads/sites/94/2019/06/BlogImage_WebDeveloper1.jpg" alt = "justin ramirez" />
-      <p>Coding student and proud dog dad.</p>
+    <section id="about-me">
+        <h2>About ME</h2>
+        <img src={JustinFinal} alt="justin ramirez" />
+        <p>Coding student and proud dog dad. Educated at Rutgers for undergrad and full stack. I enjoy helping animals 
+          keeping up to date with new technologies </p>
     </section>
-    ); 
+    );
 };
 
-export default AboutMe; 
+export default AboutMe;

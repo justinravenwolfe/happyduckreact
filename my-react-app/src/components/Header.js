04cb8668e830b24005ea8b8b825302f95f8d9657
justinravenwolfe <147559260+justinravenwolfe@users.noreply.github.com>
2024-03-29 16:25:22 -0400

    made it so it stacks in the phone screen view

--- my-react-app/src/components/Header.js
+++ my-react-app/src/components/Header.js
@@ -7,8 +7,8 @@ const Header = () => {
 //HTML 
     return (
         <header>
-        <Navbar bg = "dark" variant = "dark">
-            <Navbar.Brand>Happy Duck</Navbar.Brand>
+        <Navbar bg = "dark" variant = "dark" id = "header-responsive">
+            <Navbar.Brand><strong>Happy Duck</strong></Navbar.Brand>
             <img src = "https://upload.wikimedia.org/wikipedia/en/thumb/1/1e/Baseball_%28crop%29.jpg/1200px-Baseball_%28crop%29.jpg" alt = "baseball" height = "30" width = "30"></img>
             <li><a href = "/">About Me</a></li> 
             <li><a href = "/portfolio">Portfolio</a></li>
